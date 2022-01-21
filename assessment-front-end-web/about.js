08a4b56d86689305beb1a0bb51740e121ced0cef
GeTtygreen <94148141+GeTtygreen@users.noreply.github.com>
2022-01-21 12:27:19 -0600

    finished

--- assessment-front-end-web/about.js
+++ assessment-front-end-web/about.js
@@ -5,10 +5,22 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('i just told them what you said ')
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+//image message alert 
+
+// function picMessage(evt){
+// 	evt.preventDefault();
+
+// 	alert('HEY I JSUT WANT YOU TO KNOW YOU LOOK AMAZING')
+// }
+
+let image = document.getElementById('garfield')
+image.addEventListener('mouseover',()=>{
+	alert('HEY I JUST WANT YOU TO KNOW YOU LOOK AMAZING')
+});
---
+++ assessment-front-end-web/profile.js
@@ -0,0 +1,12 @@
+let favColor = document.getElementById('color')
+favColor.addEventListener('click',()=>{
+	alert('GREEN &#128154;')
+});
+let favPlace = document.getElementById('place')
+favPlace.addEventListener('click',()=>{
+	alert('ANYWHERE IN FRANCE')
+});
+let favRitual = document.getElementById('ritual')
+favRitual.addEventListener('click',()=>{
+	alert('PLAYING COD OR CODING WHILE EATING CHOCLATE')
+});
